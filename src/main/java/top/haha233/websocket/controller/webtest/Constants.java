072864e3744cb466db571c137485663e4d1b5717
Bigduck_VII <zzxtz007@gmail.com>
2018-06-11 11:07:44 +0800

    fix bug
    修改

---
+++ src/main/java/top/haha233/websocket/controller/webtest/Constants.java
@@ -0,0 +1,7 @@
+package top.haha233.websocket.controller.webtest;
+
+public enum Constants {
+    ;
+    public static final String WEBSOCKET_USERNAME ="w_username";
+    public static final String SESSION_USERNAME = "s_username";
+}
